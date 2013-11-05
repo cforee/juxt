f4e9e0a2c79d7ccf9d5133bebe98b3c8f54afd91
Christopher S. Foree <cforee@gmail.com>
2013-11-05 14:24:30 -0800

    Update juxt.py

--- juxt.py
+++ juxt.py
@@ -10,17 +10,17 @@ allowed_syntactic_categories = ['adj','adv','art','nou','pre','pro','ver']
 
 args = sys.argv[1:]
 
-# show help if no commang line arguments were provided
+# show help if no command line arguments were provided
 if len(args) < 1: 
   print 'USAGE: "python juxt.py ' + ' '.join(allowed_syntactic_categories) + '"'
 
 # iterate through each command line argument, select a word at random from
-# each specified syntactic category, then append it to our "sentence"
+# each specified syntactic category, then append it to our word set ("sentence")
 for arg in args:
   short_arg = arg[0:3]
   word_file = './syntactic_categories/' + short_arg + '.txt'
   if short_arg not in allowed_syntactic_categories: continue
   with open (word_file, "r") as word_list: word_set.append(random.choice(word_list.read().splitlines()))
 
-# output the "sentence"
+# output the word set
 print ' '.join(word_set)
